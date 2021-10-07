33a8da21459d91aeb4b54ed625ce0de244aa5c58
Eric Lombardo <elombardo@bkom.com>
2021-10-06 20:56:07 -0400

    Added solution for ComplexNumber Class challenge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -90,5 +90,19 @@ public class Main {
         floor = new Floor(5.4, 4.5);
         calculator1 = new Calculator(floor, carpet);
         System.out.println("total= " + calculator1.getTotalCost()); // 36.45
+
+        // TEST for ComplexNumber Class
+        System.out.println("-------------------- ComplexNumber -----------------------");
+        ComplexNumber one = new ComplexNumber(1.0, 1.0);
+        ComplexNumber number = new ComplexNumber(2.5, -1.5);
+        one.add(1,1);
+        System.out.println("one.real= " + one.getReal()); // 2.0
+        System.out.println("one.imaginary= " + one.getImaginary()); // 2.0
+        one.subtract(number);
+        System.out.println("one.real= " + one.getReal()); // -0.5
+        System.out.println("one.imaginary= " + one.getImaginary()); // 3.5
+        number.subtract(one);
+        System.out.println("number.real= " + number.getReal()); // 3.0
+        System.out.println("number.imaginary= " + number.getImaginary()); // -5.0
     }
 }
